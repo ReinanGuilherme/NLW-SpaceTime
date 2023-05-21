3751dbbff4bdbc29f74ec4716c987c18b1324409
ReinanGuilherme <r.guilhermedeoliveira3dev@gmail.com>
2023-05-21 15:58:29 -0300

    aula 04

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -1,45 +1,25 @@
-import { StatusBar } from 'expo-status-bar'
-import { ImageBackground, Text, View, TouchableOpacity } from 'react-native'
-import { useRouter } from 'expo-router'
-
-import {
-  useFonts,
-  Roboto_400Regular,
-  Roboto_700Bold,
-} from '@expo-google-fonts/roboto'
-
-import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree'
-
-import blurBg from '../src/assets/bg-blur.png'
-import Stripes from '../src/assets/stripes.svg'
-import NLWLogo from '../src/assets/nlw-spacetime-logo.svg'
-import { styled } from 'nativewind'
-import { makeRedirectUri, useAuthRequest } from 'expo-auth-session'
 import { useEffect } from 'react'
-import { api } from '../src/lib/api'
+import { useRouter } from 'expo-router'
+import { Text, TouchableOpacity, View } from 'react-native'
+import { makeRedirectUri, useAuthRequest } from 'expo-auth-session'
 import * as SecureStore from 'expo-secure-store'
 
-const StyledStripes = styled(Stripes)
+import NLWLogo from '../src/assets/nlw-spacetime-logo.svg'
+import { api } from '../src/lib/api'
 
 const discovery = {
   authorizationEndpoint: 'https://github.com/login/oauth/authorize',
   tokenEndpoint: 'https://github.com/login/oauth/access_token',
   revocationEndpoint:
-    'https://github.com/settings/connections/applications/df20fce0cccff5f58718',
+    'https://github.com/settings/connections/applications/d26f194cc5d5132a51be',
 }
 
 export default function App() {
   const router = useRouter()
 
-  const [hasLoadedFonts] = useFonts({
-    Roboto_400Regular,
-    Roboto_700Bold,
-    BaiJamjuree_700Bold,
-  })
-
   const [, response, signInWithGithub] = useAuthRequest(
     {
-      clientId: 'df20fce0cccff5f58718',
+      clientId: 'd26f194cc5d5132a51be',
       scopes: ['identity'],
       redirectUri: makeRedirectUri({
         scheme: 'nlwspacetime',
@@ -75,19 +55,11 @@ export default function App() {
     }
   }, [response])
 
-  if (!hasLoadedFonts) {
-    return null
-  }
   return (
-    <ImageBackground
-      source={blurBg}
-      className="relative flex-1 bg-gray-900 px-8 py-10"
-      imageStyle={{ position: 'absolute', left: '-100%' }}
-    >
-      <StyledStripes className="absolute left-2" />
-
+    <View className="flex-1 items-center px-8 py-10">
       <View className="flex-1 items-center justify-center gap-6">
         <NLWLogo />
+
         <View className="space-y-2">
           <Text className="text-center font-title text-2xl leading-tight text-gray-50">
             Sua cápsula do tempo
@@ -97,13 +69,14 @@ export default function App() {
             quiser) com o mundo!
           </Text>
         </View>
+
         <TouchableOpacity
           activeOpacity={0.7}
           className="rounded-full bg-green-500 px-5 py-2"
           onPress={() => signInWithGithub()}
         >
           <Text className="font-alt text-sm uppercase text-black">
-            Cadastrar Lembrança
+            Cadastrar lembrança
           </Text>
         </TouchableOpacity>
       </View>
@@ -111,8 +84,6 @@ export default function App() {
       <Text className="text-center font-body text-sm leading-relaxed text-gray-200">
         Feito com 💜 no NLW da Rocketseat
       </Text>
-
-      <StatusBar style="light" />
-    </ImageBackground>
+    </View>
   )
 }
--- mobile/src/lib/api.ts
+++ mobile/src/lib/api.ts
@@ -1,5 +1,6 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: 'http://192.168.2.204:3333',
+  // baseURL: 'http://192.168.2.204:3333',
+  baseURL: 'http://192.168.0.111:3333',
 })
